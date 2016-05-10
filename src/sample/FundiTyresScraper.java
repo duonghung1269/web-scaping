d4d33a5d8a94aa0b839a328039b00adf23f8376e
Dang Duong Hung <hungduong.dang@cimb.com>
2016-05-10 13:30:33 +0800

    - update fundi craper

--- src/sample/FundiTyresScraper.java
+++ src/sample/FundiTyresScraper.java
@@ -13,11 +13,11 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class FundiTyresScraper {
 	private final static String URL = "http://www.fundityres.co.za/tyres?id=16&tyre_brand=&tyre_profile=&tyre_quality=&tyre_rimsize=&tyre_width=&p=";
 	private static final int NO_TYRES_TEST = 2;
-	private static final int MAX_PAGE = 184;  // 184
+	private static final int MAX_PAGE = 184; // 184
 	private int pageNum = 1;
-	
+
 	private WebDriver webDriver;
-	
+
 	public FundiTyresScraper() {
 		System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");
 		webDriver = new ChromeDriver();
@@ -25,14 +25,14 @@ public class FundiTyresScraper {
 
 	public void openSite(String url) {
 		webDriver.get(url);
-		
+
 		try {
 			webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		} catch (Exception e) {
 			throw new IllegalStateException("Can't start Web Driver", e);
 		}
 	}
-	
+
 	public List<TyresCollection> getTyresCollectionList() {
 		List<TyresCollection> tyresCollectionList = new ArrayList<>();
 		int productId = 1;
@@ -43,49 +43,123 @@ public class FundiTyresScraper {
 					.xpath("//div[@class='category-products']/ol/li"));
 			for (int itemIndex = 0; itemIndex < productElements.size(); itemIndex++) {
 				WebElement element = productElements.get(itemIndex);
-				
+
 				String productUrl = element.findElement(
 						By.xpath("./a[contains(@class, 'product-image')]"))
 						.getAttribute("href");
 
 				String imageURL = element.findElement(
-						By.xpath("./a[contains(@class, 'product-image')]/img")).getAttribute("src");
-				
-				TyresCollection tyresCollection = new TyresCollection(null, productUrl, imageURL);
+						By.xpath("./a[contains(@class, 'product-image')]/img"))
+						.getAttribute("src");
+
+				TyresCollection tyresCollection = new TyresCollection(null,
+						productUrl, imageURL);
 				tyresCollectionList.add(tyresCollection);
 				tyresCollection.setId(productId++);
 			}
 		}
-		
-		
+
 		return tyresCollectionList;
 	}
-	
+
 	public void doUpdateTyres(TyresCollection tyresCollection) {
 		openSite(tyresCollection.getPageUrl());
-		String productName = webDriver.findElements(By.xpath("//div[contains(@class, 'product-shop')]//div[@class='product-name']/h1")).get(0).getText().trim();
+		String productName = webDriver
+				.findElements(
+						By.xpath("//div[contains(@class, 'product-shop')]//div[@class='product-name']/h1"))
+				.get(0).getText().trim();
 		tyresCollection.setName(productName);
-		
-		
-		String sku = webDriver.findElements(By.xpath("//table[@id='product-attribute-specs-table']//tr[position() = 1]/td[position() = 1]")).get(0).getText().trim();
-		String branch = webDriver.findElements(By.xpath("//table[@id='product-attribute-specs-table']//tr[position() = 2]/td[position() = 1]")).get(0).getText().trim();
-		String width = webDriver.findElements(By.xpath("//table[@id='product-attribute-specs-table']//tr[position() = 4]/td[position() = 1]")).get(0).getText().trim();
-		String profile = webDriver.findElements(By.xpath("//table[@id='product-attribute-specs-table']//tr[position() = 5]/td[position() = 1]")).get(0).getText().trim();
-		String rimSize = webDriver.findElements(By.xpath("//table[@id='product-attribute-specs-table']//tr[position() = 6]/td[position() = 1]")).get(0).getText().trim();
-		String loadIndexSI = webDriver.findElements(By.xpath("//div[@class='tyre-specs-inner']//span[@class='tyre-li-se']")).get(0).getText().trim();
-		
+
+		String sku = "";
+		String branch = "";
+		String width = "";
+		String profile = "";
+		String rimSize = "";
+		String loadIndexSI = "";
+		String tyreSI = "";
+
+			List<WebElement> rowElements = webDriver
+			.findElements(
+					By.xpath("//table[@id='product-attribute-specs-table']//tr"));
+			
+			for (WebElement rowElement : rowElements) {
+				String key = rowElement.findElement(By.xpath("./td[position() = 1]")).getText().trim().toLowerCase();
+				String value = rowElement.findElement(By.xpath("./td[position() = 2]")).getText().trim();
+				switch (key) {
+				case "product sku":
+					sku = value;
+					break;
+				case "tyre branch":
+					branch = value;
+					break;
+				case "tyre width":
+					width = value;
+					break;
+				case "tyre profile":
+					profile = value;
+					break;
+				case "rim size":
+					rimSize = value;
+					break;
+				case "load index":
+					loadIndexSI = value;
+					break;
+				case "tyre si":
+					tyreSI = value;
+					break;
+				default: 
+						break;
+				}
+			}
+			
+			
+
+//		String sku = webDriver
+//				.findElements(
+//						By.xpath("//table[@id='product-attribute-specs-table']//tr[position() = 1]/td[position() = 1]"))
+//				.get(0).getText().trim();
+//		String branch = webDriver
+//				.findElements(
+//						By.xpath("//table[@id='product-attribute-specs-table']//tr[position() = 2]/td[position() = 1]"))
+//				.get(0).getText().trim();
+//		String width = webDriver
+//				.findElements(
+//						By.xpath("//table[@id='product-attribute-specs-table']//tr[position() = 4]/td[position() = 1]"))
+//				.get(0).getText().trim();
+//		String profile = webDriver
+//				.findElements(
+//						By.xpath("//table[@id='product-attribute-specs-table']//tr[position() = 5]/td[position() = 1]"))
+//				.get(0).getText().trim();
+//		String rimSize = webDriver
+//				.findElements(
+//						By.xpath("//table[@id='product-attribute-specs-table']//tr[position() = 6]/td[position() = 1]"))
+//				.get(0).getText().trim();
+//		String loadIndexSI = webDriver
+//				.findElements(
+//						By.xpath("//div[@class='tyre-specs-inner']//span[@class='tyre-li-se']"))
+//				.get(0).getText().trim();
+
 		String loadIndex = "";
 		if (loadIndexSI.length() < 2) {
 			loadIndex = "";
 		} else {
-			loadIndex = loadIndexSI.substring(0, loadIndexSI.length() - 1); // remove last SI character
+			loadIndex = loadIndexSI.substring(0, loadIndexSI.length() - 1); // remove
+																			// last
+																			// SI
+																			// character
 		}
-		
-		String tyreSI = webDriver.findElements(By.xpath("//table[@id='product-attribute-specs-table']//tr[position() = 7]/td[position() = 1]")).get(0).getText().trim();
-		String price = webDriver.findElements(By.xpath("//div[@class='tyre-specs-inner']//span[@class='boldprice']")).get(0).getText().trim();
-		
+
+//		String tyreSI = webDriver
+//				.findElements(
+//						By.xpath("//table[@id='product-attribute-specs-table']//tr[position() = 7]/td[position() = 1]"))
+//				.get(0).getText().trim();
+		String price = webDriver
+				.findElements(
+						By.xpath("//div[@class='tyre-specs-inner']//span[@class='boldprice']"))
+				.get(0).getText().trim();
+
 		tyresCollection.setBranch(branch);
-		
+
 		Tyres tyres = new Tyres();
 		tyres.setLoadIndex(loadIndex);
 		tyres.setPrice(price);
@@ -94,14 +168,14 @@ public class FundiTyresScraper {
 		tyres.setSize(rimSize);
 		tyres.setSku(sku);
 		tyres.setWidth(width);
-		
+
 		tyresCollection.getTyresList().add(tyres);
 	}
-	
+
 	private void closeBrowser() {
-		//close the browser
-	    webDriver.close();
-	    webDriver.quit();
+		// close the browser
+		webDriver.close();
+		webDriver.quit();
 	}
 
 	public void doUpdateTyresList(List<TyresCollection> tyresCollectionList) {
@@ -110,18 +184,20 @@ public class FundiTyresScraper {
 			doUpdateTyres(tyresCollection);
 		}
 	}
-	
+
 	public static void main(String[] args) throws IOException {
 		FundiTyresScraper scrapper = new FundiTyresScraper();
 		scrapper.openSite(URL + scrapper.pageNum++);
-		List<TyresCollection> tyresCollectionList = scrapper.getTyresCollectionList();
-		
-		ExcelUtil.marshalToXML(tyresCollectionList, "FundiTyresParsedAllUrl.xml");
-		
+		List<TyresCollection> tyresCollectionList = scrapper
+				.getTyresCollectionList();
+
+		ExcelUtil.marshalToXML(tyresCollectionList,
+				"FundiTyresParsedAllUrl.xml");
+
 		scrapper.doUpdateTyresList(tyresCollectionList);
 
 		ExcelUtil.marshalToXML(tyresCollectionList, "FundiTyresParsed.xml");
-		
+
 		ExcelUtil.exportToExcel(tyresCollectionList, "FundiTyres.xls");
 		scrapper.closeBrowser();
 	}
